52aa5a000d77a5e4f9699baed919eb404fa6cf4c
Sude Nur Komur <sudenurkomur1253@gmail.com>
2024-05-09 22:26:22 +0300

    Trying with Jar File

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import enigma.core.Enigma;
+import enigma.event.TextMouseEvent;
+import enigma.event.TextMouseListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import enigma.console.TextAttributes;
+import java.awt.Color;
+
 public class Main {
+
+    public static enigma.console.Console cn = Enigma.getConsole("Mouse and Keyboard");
+
     public static void main(String[] args) {
+
+        cn.getTextWindow().setCursorPosition(1,0);
+        cn.getTextWindow().output("-- WELCOME TO THE MAZE GAME --");
+        System.out.println();
 
         //start the game
 
